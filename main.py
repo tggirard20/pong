d96723f7dbe6beb3e0bace06d5aa550646b07f49
tggirard <tristan.girard16@gmail.com>
2023-03-20 18:11:34 -0700

    first commit

---
+++ main.py
@@ -0,0 +1,36 @@
+# This is a sample Python script.
+
+# Press ⌃R to execute it or replace it with your code.
+# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+
+from turtle import Screen, Turtle
+
+screen =Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height= 600)
+screen.title("Pong")
+
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5,stretch_len=1)
+paddle.penup()
+paddle.goto(350,0)
+
+def go_up():
+    new_y = paddle.ycor() + 20
+    paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    new_y = paddle.ycor() - 20
+    paddle.goto(paddle.xcor(), new_y)
+
+
+screen.listen()
+screen.onkey(go_up(), "Up")
+screen.onkey(go_down(), "Down")
+
+
+screen.exitonclick()
+
